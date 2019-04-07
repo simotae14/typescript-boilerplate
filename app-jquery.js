7ba9be9c9d90c3d82bea4e1325f82233ca163d31
Simone Taeggi <startae14@gmail.com>
2019-04-07 19:20:01 +0200

    typescript using jQuery

---
+++ app-jquery.js
@@ -0,0 +1,6 @@
+"use strict";
+// Declare $ as a variable
+//declare var $: any;
+$('button').click(function () {
+    alert('Button was clicked!');
+});
---
+++ app-jquery.ts
@@ -0,0 +1,6 @@
+// Declare $ as a variable
+//declare var $: any;
+
+$('button').click(function() {
+    alert('Button was clicked!');
+});
